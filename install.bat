e0ca890e503aa4eab08fe73db7030a2746e49825
Brian Yip <itsbriany@gmail.com>
2016-09-03 16:12:32 -0300

    Adding the install script

--- install.bat
+++ install.bat
@@ -1,4 +1,12 @@
 git submodule update --init --recursive
+
+cd %GAME_ENGINE_ROOT%\protobuf\cmake
+cmake -Dprotobuf_BUILD_TESTS=OFF -G "Visual Studio 14"
+msbuild /m protobuf.sln /p:Configuration=Release /p:Platform="Win32"
+md %GAME_ENGINE_ROOT%\tools
+xcopy /s Release\* %GAME_ENGINE_ROOT%\tools
+
+
 cd %GOOGLE_TEST_DISTRIBUTION%
 cmake -DBUILD_GMOCK:BOOL=ON -DBUILD_SHARED_LIBS:BOOL=ON -G "Visual Studio 14"
 msbuild /m googletest-distribution.sln /p:Configuration=Debug /p:Platform="Win32"
