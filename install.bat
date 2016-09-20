4c6add77bbeb7ab1edd1b80e9b0e788b535c14ec
Brian Yip <itsbriany@gmail.com>
2016-09-20 12:07:35 -0300

    Tests should now run and pass on appveyor

--- install.bat
+++ install.bat
@@ -4,7 +4,7 @@ cd %PROTOBUF_ROOT%\cmake
 cmake -Dprotobuf_BUILD_TESTS=OFF -G "Visual Studio 14"
 msbuild /m protobuf.sln /p:Configuration=Release /p:Platform="Win32"
 md %GAME_ENGINE_ROOT%\Tools
-xcopy /s Release\* %GAME_ENGINE_ROOT%\Tools
+xcopy /s Release\protoc.exe %GAME_ENGINE_ROOT%\Tools
 
 cd %GAME_ENGINE_ROOT%
 Tools\protoc --proto_path=GameEngine\Common\GameEngineCommon\ --cpp_out=GameEngine\Common GameEngine\Common\GameEngineCommon\Chat.proto
@@ -13,13 +13,6 @@ cd %GOOGLE_TEST_DISTRIBUTION%
 cmake -DBUILD_GMOCK:BOOL=ON -DBUILD_GTEST:BOOL=ON -DBUILD_SHARED_LIBS:BOOL=ON -G "Visual Studio 14"
 msbuild /m googletest-distribution.sln /p:Configuration=Debug /p:Platform="Win32" /p:GTEST_CREATE_SHARED_LIBRARY=1
 msbuild /m googletest-distribution.sln /p:Configuration=Release /p:Platform="Win32" /p:GTEST_CREATE_SHARED_LIBRARY=1
-cd %GAME_ENGINE_ROOT%\GameEngine
-md Debug
-md Release
-xcopy /s %GOOGLE_TEST_DISTRIBUTION%\googlemock\gtest\Debug\* Debug
-xcopy /s %GOOGLE_TEST_DISTRIBUTION%\googlemock\gtest\Release\* Release
-xcopy /s %GOOGLE_TEST_DISTRIBUTION%\googlemock\Debug\* Debug
-xcopy /s %GOOGLE_TEST_DISTRIBUTION%\googlemock\Release\* Release
 
 cd %GOOGLE_TEST_DISTRIBUTION%\GameEngine
 cmake -G "Visual Studio 14"
