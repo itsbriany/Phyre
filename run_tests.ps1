f71d4e2041993e92d418f385268c19fca4221e11
Brian Yip <itsbriany@gmail.com>
2016-11-02 00:23:55 -0300

    AppVeyor should recognize failed tests

--- run_tests.ps1
+++ run_tests.ps1
@@ -2,13 +2,24 @@ param (
     [switch]$Release = $false
 )
 
-# For appveyor CI
 if ($env:CONFIGURATION -eq "Release") {
     $Release = $true
+}
+
+# Generate xml report for AppVeyor
+if ($env:APPVEYOR) {
+    $env:GTEST_OUTPUT = "xml"
 }
 
 if ($release) {
     Get-ChildItem -Path ThirdParty\googletest\googlemock\Release -Filter *.exe | % {& $_.FullName}
 } else {
     Get-ChildItem -Path ThirdParty\googletest\googlemock\Debug -Filter *.exe | % {& $_.FullName}
+}
+
+# upload results to AppVeyor
+if ($env:APPVEYOR) {
+    Resolve-Path $env:GAME_ENGINE_ROOT\test_detail.xml
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $env:GAME_ENGINE_ROOT\test_detail.xml))
 }
