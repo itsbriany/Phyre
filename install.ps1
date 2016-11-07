ed1fd543ee25b86c5ce20d14869670a728957250
Brian Yip <itsbriany@gmail.com>
2016-11-07 11:57:23 -0400

    Base64 on Windows

--- install.ps1
+++ install.ps1
@@ -3,6 +3,7 @@ Import-Module $env:GAME_ENGINE_ROOT\Build.psm1 -Force -DisableNameChecking
 function Install-GameEngine() {
   Init-Submodules
   Set-VS140-Environment
+  Build-Base64
   Build-Protobuf
   Create-Tools
   Compile-Protobuf
