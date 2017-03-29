234afe7f03747639393b1fa301777323f23228aa
Brian Yip <itsbriany@gmail.com>
2017-03-29 20:09:43 -0300

    Trying to explicitly build solution file

--- Tools/install.bat
+++ Tools/install.bat
@@ -1,15 +1,18 @@
 set PATH=C:\Python35;C:\Python35\Scripts;%PATH%
 git submodule update --init --recursive
 
-pushd %PHYRE_ROOT%\ThirdParty\Vulkan-LoaderAndValidationLayers
-update_external_sources.bat --all
-build_windows_targets.bat 64 %CONFIGURATION%
-popd
+REM pushd %PHYRE_ROOT%\ThirdParty\Vulkan-LoaderAndValidationLayers
+REM update_external_sources.bat --all
+REM build_windows_targets.bat 64 %CONFIGURATION%
+REM popd
 
 pushd %PHYRE_ROOT%
+dir
 mkdir Build
 pushd Build
+dir
 cmake -G "Visual Studio 14 Win64" ..
 cmake --build . --target ALL_BUILD --config %CONFIGURATION%
+dir
 popd
 popd
