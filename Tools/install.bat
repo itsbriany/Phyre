bda61eb3cb675a25a8c21414979b1244dfc4a4c3
Brian Yip <itsbriany@gmail.com>
2017-03-29 18:46:37 -0300

    Vulkan is now included in the build

--- Tools/install.bat
+++ Tools/install.bat
@@ -1,5 +1,11 @@
-set
+set PATH=C:\Python35;C:\Python35\Scripts;%PATH%
 git submodule update --init --recursive
+
+pushd %PHYRE_ROOT%\ThirdParty\Vulkan-LoaderAndValidationLayers
+update_external_sources.bat --all
+build_windows_targets.bat
+popd
+
 pushd %PHYRE_ROOT%
 mkdir Build
 pushd Build
